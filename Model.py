174a4ace35f1e03ef6b5bfc32fde04c2088e14a1
AhmedFakhry47 <ahmedfakhry805@gmail.com>
2021-02-18 11:18:16 +0200

    Altered model design

--- Model.py
+++ Model.py
@@ -6,11 +6,16 @@ import json
 class Shallow_M (tf.keras.Model):
   def __init__(self,n_classes=1,act='sig'):
     super(Shallow_M,self).__init__()
-    self.DenseA   = tf.keras.layers.Dense(64,activation=tf.nn.tanh)
+    
+    self.DenseP   = tf.keras.layers.Dense(256,activation=tf.nn.relu)
+    self.BatchN_P = tf.keras.layers.BatchNormalization(momentum=0.9)
+    self.DropO_P  = tf.keras.layers.Dropout(rate=0.5)    
+    
+    self.DenseA   = tf.keras.layers.Dense(256,activation=tf.nn.relu)
     self.BatchN_A = tf.keras.layers.BatchNormalization(momentum=0.9)
     self.DropO_A  = tf.keras.layers.Dropout(rate=0.5)    
     
-    self.DenseB   = tf.keras.layers.Dense(64,activation=tf.nn.tanh)
+    self.DenseB   = tf.keras.layers.Dense(256,activation=tf.nn.relu)
     self.BatchN_B = tf.keras.layers.BatchNormalization(momentum=0.9)
     self.DropO_B  = tf.keras.layers.Dropout(rate=0.5)    
     
@@ -20,7 +25,12 @@ class Shallow_M (tf.keras.Model):
       self.Predict  = tf.keras.layers.Dense(n_classes,activation=tf.nn.softmax)
 
   def call(self,inputs,training=False):
-    x = tf.math.reduce_mean(inputs,axis=-1)
+    x = tf.math.reduce_mean(inputs,axis=1)
+    
+    p = self.DenseP(inputs[:,0,:])
+    p = self.BatchN_P(p)
+    p = self.DropO_P(p)
+
     x = self.DenseA(x)
     x = self.BatchN_A(x,)
     x = self.DropO_A(x,training=training)
@@ -29,5 +39,8 @@ class Shallow_M (tf.keras.Model):
     x = self.BatchN_B(x,)
     x = self.DropO_B(x,training=training)
 
-    return self.Predict(x)
+    out = tf.keras.layers.concatenate([x,p])
+
+    return self.Predict(out)
+  
   
