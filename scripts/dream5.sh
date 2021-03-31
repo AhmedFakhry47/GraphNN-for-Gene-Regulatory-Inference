56850895b35348408ebace10aaea42cabdb60a4a
AhmedFakhry47 <ahmedfakhry805@gmail.com>
2021-03-31 07:35:02 +0200

    scripts

---
+++ scripts/dream5.sh
@@ -0,0 +1,5 @@
+!/bin/bash
+mkdir '/content/dream5'
+mv '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/Yeastract_Preprocessed.json.tar.gz' '/content/dream5/Yeastract_Preprocessed.json.tar.gz'
+rm -R '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/'
+tar -xf '/content/yeastract/Yeastract_Preprocessed.json.tar.gz'
---
+++ scripts/yeastract.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir '/content/yeastract'
+mv '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/yeastract_w_GT.tar.bz2' '/content/yeastract/yeastract_w_GT.tar.bz2'
+rm -R '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/'
+tar -xf '/content/yeastract/yeastract_w_GT.tar.bz2'
