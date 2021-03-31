903988517fb4f2f6ba6bb8ec89659348ebd91093
AhmedFakhry47 <ahmedfakhry805@gmail.com>
2021-03-31 10:51:23 +0200

    Finished Scripts

--- scripts/dream5.sh
+++ scripts/dream5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir '/content/dream5'
-mv '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/Yeastract_Preprocessed.json.tar.gz' '/content/dream5/Yeastract_Preprocessed.json.tar.gz'
+mv '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/Preprocessed.json.tar.gz' '/content/dream5/Preprocessed.json.tar.gz'
 rm -R '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/'
 cd '/content/dream5'
-tar -xf '/content/yeastract/Yeastract_Preprocessed.json.tar.gz'
+tar -xf '/content/yeastract/Preprocessed.json.tar.gz'
 cd ..
