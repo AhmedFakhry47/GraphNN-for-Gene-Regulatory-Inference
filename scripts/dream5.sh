c1dd5545664de4c3eb4a991cd0c5d12405491a27
AhmedFakhry47 <ahmedfakhry805@gmail.com>
2021-04-02 05:31:19 +0200

    Hopefully last update for scripts

--- scripts/dream5.sh
+++ scripts/dream5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir '/content/dream5'
 mv '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/Preprocessed.json.tar.gz' '/content/dream5/Preprocessed.json.tar.gz'
-rm -R '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/'
+#rm -R '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/'
 cd '/content/dream5'
 tar -xf '/content/dream5/Preprocessed.json.tar.gz'
 rm '/content/dream5/Preprocessed.json.tar.gz'
--- scripts/yeastract.sh
+++ scripts/yeastract.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir '/content/yeastract'
 mv '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/yeastract_w_GT.tar.bz2' '/content/yeastract/yeastract_w_GT.tar.bz2'
-rm -R '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/'
+#rm -R '/content/Regulatory-Gene-Network-Inferance-from-gene-exepression-data/'
 cd '/content/yeastract'
 tar -xf '/content/yeastract/yeastract_w_GT.tar.bz2'
 rm '/content/yeastract/yeastract_w_GT.tar.bz2'
